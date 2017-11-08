2625baae86b9d6aee11c0dc3874a56c553da4634
Glitch (dilbert-api) <none>
2017-11-08 20:33:57 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -20,6 +20,25 @@ nunjucks.configure('views', {
 });
 
 
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+
 app.get('/', (req, res) => {
   axios.get(url).then(response => {
     const $ = cheerio.load(response.data);
