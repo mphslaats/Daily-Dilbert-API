f24ad43dc60c9a375cb819c79bf01cf35a5573b4
Glitch (dilbert-api) <none>
2017-11-08 16:26:34 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -2,28 +2,38 @@ const express = require('express');
 const app = express();
 const axios = require('axios');
 const cheerio = require('cheerio');
-let today_response = {};
+const nunjucks = require('nunjucks');
 
+const today = new Date();
+let year = today.getFullYear();
+let month = today.getMonth() + 1
+month = month < 10 ? `0${month}`: month;
+let day = today.getDate();
+day = day < 10 ? `0${day}`: day;
+const date = `${year}-${month}-${day}`;
+
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});
+
+
+app.get('/', (req, res) => {
+  res.render('index.html', { today: 'bar' });
+});
 
 app.get('/', (request, response) => {
   response.sendFile(`${__dirname}/views/index.html`);
 });
 
 
-app.get('/today', (req, res) => {
-  const today = new Date();
-  let year = today.getFullYear();
-  let month = today.getMonth() + 1
-  month = month < 10 ? `0${month}`: month;
-  let day = today.getDate();
-  day = day < 10 ? `0${day}`: day;
-  var date = `${year}-${month}-${day}`;
+app.get('/json', (req, res) => {
   var url = `http://dilbert.com/strip/${date}`
   axios.get(url).then(response => {
     const $ = cheerio.load(response.data);
     const comicUrl = $('.img-comic').attr('src');
-    today_response = {"today": `${comicUrl}.png`};
-    res.json(today_response);
+    res.json({"today": `${comicUrl}.png`});
   });
 });
 
