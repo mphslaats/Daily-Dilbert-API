201297a1f29e4ea356d753642e7b47abf0d0d409
Glitch (dilbert-api) <none>
2017-12-06 17:33:49 +0000

    🌍⚽️ Checkpoint server.js:204461

--- server.js
+++ server.js
@@ -20,21 +20,11 @@ nunjucks.configure('views', {
 });
 
 
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
+app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
   next();
 });
 
