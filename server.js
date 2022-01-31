1c9f1285558463da794bf39f628777f01e165522
Glitch (dilbert-api) <none>
2022-01-31 19:29:04 +0000

    🌧🏔 Checkpoint ./server.js:41723650/3451

--- server.js
+++ server.js
@@ -1,60 +1,64 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const axios = require('axios');
-const cheerio = require('cheerio');
-const nunjucks = require('nunjucks');
+const axios = require("axios");
+const cheerio = require("cheerio");
+const nunjucks = require("nunjucks");
 
 const today = new Date();
 const year = today.getFullYear();
-let month = today.getMonth() + 1
-month = month < 10 ? `0${month}`: month;
+let month = today.getMonth() + 1;
+month = month < 10 ? `0${month}` : month;
 let day = today.getDate();
-day = day < 10 ? `0${day}`: day;
+day = day < 10 ? `0${day}` : day;
 const date = `${year}-${month}-${day}`;
-const url = `http://dilbert.com/strip/${date}`
+const url = `http://dilbert.com/strip/${date}`;
 
-
-nunjucks.configure('views', {
+nunjucks.configure("views", {
   autoescape: true,
-  express: app
+  express: app,
 });
 
-
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
 
-
-app.get('/', (req, res) => {
-  axios.get(url).then(response => {
-    const $ = cheerio.load(response.data);
-    const comicTitle = $('.comic-title-name').text();
-    const comicUrl = $('.img-comic').attr('src');
-    res.render('index.html', {
-      "title": comicTitle,
-      "image": `${comicUrl}.png`
-    });
-  }).catch(error => res.json(error));
+app.get("/", (req, res) => {
+  axios
+    .get(url)
+    .then((response) => {
+      const $ = cheerio.load(response.data);
+      const comicTitle = $(".comic-title-name").text();
+      const comicUrl = $(".img-comic").attr("src");
+      res.render("index.html", {
+        title: comicTitle,
+        image: `${comicUrl}.png`,
+      });
+    })
+    .catch((error) => res.json(error));
 });
 
-
-app.get('/json', (req, res) => {
-  axios.get(url).then(response => {
-    const $ = cheerio.load(response.data);
-    const comicTitle = $('.comic-title-name').text();
-    const comicUrl = $('.img-comic').attr('src');
-    const jsonResponse = {
-      "title": comicTitle,
-      "image": `${comicUrl}.png`,
-    };
-    res.json(jsonResponse);
-  }).catch(error => res.json(error));
+app.get("/json", (req, res) => {
+  axios
+    .get(url)
+    .then((response) => {
+      const $ = cheerio.load(response.data);
+      const comicTitle = $(".comic-title-name").text();
+      const comicUrl = $(".img-comic").attr("src");
+      const jsonResponse = {
+        title: comicTitle,
+        image: `${comicUrl}.png`,
+      };
+      res.json(jsonResponse);
+    })
+    .catch((error) => res.json(error));
 });
-
 
 const listener = app.listen(process.env.PORT, () => {
   console.log(`Your app is listening on port ${listener.address().port}`);
